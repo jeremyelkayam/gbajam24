7b266de75e9c74065b80cc9b095f9ae7e53022b0
Jeremy Elkayam <jeremyelkayam@gmail.com>
2024-05-19 12:54:03 -0400

    the basics are here now

--- src/main.cpp
+++ src/main.cpp
@@ -10,23 +10,6 @@
 #include "bn_regular_bg_items_testbg.h"
 #include "player.h"
 
-//p is the progress of the ease, represented as a fraction of total time
-bn::fixed ease(bn::fixed p)
-{
-    //linear ease
-    // return p;
-
-	// if(p < 0.5)
-	// {
-	// 	return 2 * p * p;
-	// }
-	// else
-	// {
-
-    //quadratic ease out
-	return -(p * (p - 2));
-	// }
-}
 
 int main()
 {
@@ -35,43 +18,31 @@ int main()
     bn::bg_palettes::set_transparent_color(bn::color(25, 25, 25));
     bn::camera_ptr cam = bn::camera_ptr::create(0,0);    bg.set_camera(cam);
     aru::player player(cam, 0, 0, 2, 0.5);
-    bn::fixed_point cam_offset(40 * (player.facing_left() ? 1 : -1), -30);
-    unsigned short timer,ease_time;
-    timer = -1;
-    ease_time = 20;
-
-    bn::fixed xoffset_start;
-    bn::fixed xoffset_target;
 
 
     while(true)
     {
-        bool was_facing_left = player.facing_left();
         player.update();
-        bn::fixed xoffset;
+        // bn::fixed_point target_cam_offset(40 * (player.facing_left() ? 1 : -1), -30);
 
-        // i hate this. but it's a camera ease
-        if(!was_facing_left && player.facing_left()){
-            xoffset_start = -40;
-            xoffset_target = 40;
-            timer = 0;
+        //todo: maybe add easing to this ?
+        bn::fixed xoffset = cam.x() - player.x();
+        bn::fixed target_xoffset = 50 * (player.facing_left() ? 1 : -1);
+        bn::fixed camspeed = 8;
+
+        if(xoffset < target_xoffset){
+            xoffset += camspeed;
+        }else if(xoffset > target_xoffset){
+            xoffset -= camspeed;
         }
 
-        if(was_facing_left && !player.facing_left()){
-            xoffset_start = 40;
-            xoffset_target = -40;
-            timer = 0;
+        if(target_xoffset - camspeed < xoffset && xoffset < target_xoffset + camspeed){
+            xoffset = target_xoffset;
         }
-
-        if(timer < ease_time){
-            ++timer;
-            xoffset = (xoffset_start - xoffset_target) * ease(bn::fixed(timer) / bn::fixed(ease_time)) - xoffset_start;
-            cam_offset.set_x(xoffset);
-        }
-
+        
 
         // BN_LOG("camera offset ", cam_offset);
-        cam.set_position(player.position() + cam_offset);
+        cam.set_position(player.x() + xoffset, player.y() - 30);
         bn::core::update();
     }
 }
